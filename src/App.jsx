04c1beff6c903b07f0e6ab8a0e06589519551bd6
oscarm.alvarez6 <oscarm.alvarez6@gmail.com>
2023-11-04 17:30:16 +0000

    feat: :sparkles: Login is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import './App.css'
+
 function App() {
   return (
     <>
-      <h1> Hola mundo </h1>
+      <div>
+      <div class="login-box">
+      <img src="https://clay.com.co/wp-content/uploads/2020/03/clay-bioseguridad-logo-pop-up-300x122.png" class="avatar" alt="Avatar Image"/>
+      <h1>Login Here</h1>
+      <form action="">
+      <label for="username">Username</label>
+      <input type="text" placeholder="Enter Username"/>
+      <label for="password">Password</label>
+      <input type="password" placeholder="Enter Password"/>
+      <input type="submit" value="Log In"/>
+      <a href="#">Lost your Password?</a><br/>
+      <a href="#">Don't have An account?</a>
+      </form>
+    </div>
+      </div>
     </>
   )
 }
